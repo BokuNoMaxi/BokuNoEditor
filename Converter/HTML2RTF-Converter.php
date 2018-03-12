4fc1c1e8c5ea0cb5f255d458b991b2e2cc086ab3
BokuNoMaxi <ketterer.markus@gmx.at>
2018-03-12 15:57:12 +0100

    Umbenannt zu BokuNoEditor
    Struktur neu gemacht, Designs angepasst.

--- Converter/HTML2RTF-Converter.php
+++ Converter/HTML2RTF-Converter.php
@@ -1,29 +1,6 @@
 <?php
-function makeRTF($Info,$Schriftarten,$Color){
-    $Deff= setDefinition($Info, $Schriftarten, $Color);
-    $Standards='\deflang1031\plain\fs26\widowctrl\hyphauto\ftnbj';
-    return '{\rtf1\ansi'.$Deff.$Standards.'}';
-}
-function setSchriftart($Schriftarten){
-    $DefinitionSchriftarten='';
-    for($i=0;$i<count($Schriftarten);$i++){
-        $DefinitionSchriftarten.='{\f'.$i.' '.$Schriftarten[$i].';}';
-    }
-    return '{\fonttbl'.$DefinitionSchriftarten.'}';
-}
-function setColors($Color){
-    $Farben='';
-    for($i=0;$i<=count($Color);$i++){
-        $Farben.='{\red'.$Color[$i]['red'].'\blue'.$Color[$i]['blue'].'\green'.$Color[$i]['green'].';}';
-    }
-    return '{\colortbl;'.$Farben.'}';
-}
-function setInformationen($Info){
-    return '{\info{\title '.$Info['Titel'].'}{\author '.$Info['Author'].'}{\company '.$Info['Company'].'}{\creatim\yr'.$Info['Datum']->format('Y').'\mo'.$Info['Datum']->format('m').'\dy'.$Info['Datum']->format('d').'\hr'.$Info['Datum']->format('H').'\min'.$Info['Datum']->format('i').'}{\doccomm '.$Info['Kommentar'].'}}';
-}
-function setDefinition($Info,$Schriftarten,$Color){
-    $SchriftartenDokument= setSchriftart($Schriftarten);
-    $Farben= setColors($Color);
-    $Dokumentinfo= setInformationen($Info);
-    return '\deff0'.$SchriftartenDokument.$Farben.$Dokumentinfo;
-}
+include('RTF-Formatierung.php');
+include('Sonderzeichen.php');
+include('Bilder.php');
+
+$HTML=$_POST['Content'];
--- Editor/BokuNoEditor.js
+++ Editor/BokuNoEditor.js
@@ -1,5 +1,8 @@
 (function($){
-    var lastFocus,
+    var scripts = document.getElementsByTagName("script"),
+        src = scripts[scripts.length-1].src,
+        absolutPath=src.slice(0,src.slice(0,src.lastIndexOf('/')).lastIndexOf('/')),
+        lastFocus,
         Schriftart=[
         'Arial','Arial Black',
         'Book Antiqua',
@@ -13,14 +16,24 @@
     ],Schriftgroesse=[
         '6','7','8','9','10','10.5','11','12','13','14','15','16','18','20','22','24','26','28','32','36','40','44','48','54','60','66','72',
     ];
-    
     $.fn.bokunoeditor=function(){
         var Textarea=$(this);
         $(Textarea).hide().parent().append('<div id="bokunoeditorIframe"><div id="bokunoeditorMenue"></div><div id="bokunoeditorToolbar"></div><div id="bokunoeditorContent"></div></div>');
+        $('#bokunoeditorIframe').append('<div id="bokuenoeditorMenuDateiContextmenu"><div id="bokuenoeditorMenuDateiContextmenuNeu"><button type="Button" class="bneMenuButton">Neu</button></div><div class="bneMenueTrennlinie"></div><div id="bokuenoeditorMenuDateiContextmenuAbsenden"><button type="Button" class="bneMenuButton">Absenden</button></div></div>')
         $('#bokunoeditorContent').attr('contentEditable','true').html($(Textarea).val());
-        $('#bokunoeditorMenue').html('<button type="button" class="bokunoeditorMenueButton">Datei</button><button type="button" class="bokunoeditorMenueButton">Schriftart</button><button type="button" class="bokunoeditorMenueButton">Format</button>');
+        $('#bokunoeditorMenue').html('<button type="button" class="bokunoeditorMenueButton" id="bokunoeditorDatei">Datei</button><button type="button" class="bokunoeditorMenueButton">Schriftart</button><button type="button" class="bokunoeditorMenueButton">Format</button>');
         $('#bokunoeditorToolbar').html('<button type="button" class="bokunoeditorToolbarButton" id="bokunoeditorToolbarFett">B</button><button type="button" class="bokunoeditorToolbarButton" id="bokunoeditorToolbarKursiv">I</button><select class="bokunoeditorToolbarSelect" id="bokunoeditorSchriftart"></select><select class="bokunoeditorToolbarSelect" id="bokunoeditorSchriftgroesse"></select><button class="bokunoeditorToolbarButton bokunoeditorToolbarAusrichtung" id="bokunoeditorToolbarLinks" type="button">Links</button><button type="button" class="bokunoeditorToolbarButton bokunoeditorToolbarAusrichtung" id="bokunoeditorToolbarMitte">Mitte</button><button type="button" class="bokunoeditorToolbarButton bokunoeditorToolbarAusrichtung" id="bokunoeditorToolbarRechts">Rechts</button>');
         
+        $('#bokuenoeditorMenuDateiContextmenuAbsenden button').click(function(){
+            $.post(absolutPath+'/Converter/HTML2RTF-Converter.php',{
+                Content:$('#bokunoeditorContent').html()
+            },function(){
+                
+            });
+        });
+        $('#bokunoeditorDatei').click(function(){
+            $('#bokuenoeditorMenuDateiContextmenu').toggleClass('bneOpen');
+        });
         (($('#bokunoeditorContent div').length===0)?$('#bokunoeditorContent').append('<div></div>'):'');
         $.each(Schriftart,function(index,value){
             $('#bokunoeditorSchriftart').append('<option value="'+value+'">'+value+'</option>');
@@ -28,6 +41,7 @@
         $.each(Schriftgroesse,function(index,value){
             $('#bokunoeditorSchriftgroesse').append('<option value="'+value+'" '+((value=='11')?'selected="selected"':'')+'>'+value+' pt</option>');
         });
+        
         $('.bokunoeditorToolbarButton').click(function(){
             var Button=$(this);
             if(lastFocus){
@@ -52,15 +66,15 @@
                     }
                     switch(Button[0]){
                         case bokunoeditorToolbarLinks:
-                            $(Markierung.startContainer.parentElement).css('text-align','left');
+                            $(Markierung.startContainer.parentElement).attr('align','left');
                             $('.bokunoeditorToolbarAusrichtung').removeClass('bneActive');
                             break;
                         case bokunoeditorToolbarMitte:
-                            $(Markierung.startContainer.parentElement).css('text-align','center');
+                            $(Markierung.startContainer.parentElement).attr('align','center');
                             $('.bokunoeditorToolbarAusrichtung').removeClass('bneActive');
                             break;
                         case bokunoeditorToolbarRechts:
-                            $(Markierung.startContainer.parentElement).css('text-align','right');
+                            $(Markierung.startContainer.parentElement).attr('align','right');
                             $('.bokunoeditorToolbarAusrichtung').removeClass('bneActive');
                             break;
                         default:
